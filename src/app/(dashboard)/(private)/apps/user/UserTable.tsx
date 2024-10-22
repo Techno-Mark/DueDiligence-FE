913c9d9df04e71bdaf978546d2db6f97897240ec
Alvish6654 <alvish.pipaliya@technomark.io>
2024-10-22 16:13:15 +0530

    change in table design for roles and user module

--- src/app/(dashboard)/(private)/apps/user/UserTable.tsx
+++ src/app/(dashboard)/(private)/apps/user/UserTable.tsx
@@ -269,9 +269,9 @@ const UserListTable = ({ tableData }: { tableData?: UsersType[] }) => {
   return (
     <>
       <Card>
-        <CardHeader title="User List" />
-        <CardContent>
-          <div className="flex justify-end flex-col sm:flex-row max-sm:is-full pb-6 pr-6 items-start sm:items-center gap-4">
+        <div className="flex justify-between flex-col items-start md:flex-row md:items-center p-6 border-bs gap-4">
+          <CardHeader title="User List" />
+          <div className="flex flex-col sm:flex-row max-sm:is-full items-start sm:items-center gap-4">
             <DebouncedInput
               value={globalFilter ?? ""}
               onChange={(value) => setGlobalFilter(String(value))}
@@ -288,66 +288,64 @@ const UserListTable = ({ tableData }: { tableData?: UsersType[] }) => {
               Add New User
             </Button>
           </div>
-          <div className="overflow-x-auto">
-            <table className={tableStyles.table}>
-              <thead>
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <tr key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => (
-                      <th key={header.id}>
-                        {header.isPlaceholder ? null : (
-                          <div
-                            className={classnames({
-                              "flex items-center": header.column.getIsSorted(),
-                              "cursor-pointer select-none":
-                                header.column.getCanSort(),
-                            })}
-                            onClick={header.column.getToggleSortingHandler()}
-                          >
-                            {flexRender(
-                              header.column.columnDef.header,
-                              header.getContext()
-                            )}
-                            {{
-                              asc: <i className="tabler-chevron-up text-xl" />,
-                              desc: (
-                                <i className="tabler-chevron-down text-xl" />
-                              ),
-                            }[header.column.getIsSorted() as "asc" | "desc"] ??
-                              null}
-                          </div>
-                        )}
-                      </th>
-                    ))}
-                  </tr>
-                ))}
-              </thead>
-              <tbody>
-                {table.getRowModel().rows.map((row) => (
-                  <tr key={row.id}>
-                    {row.getVisibleCells().map((cell) => (
-                      <td key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <TablePagination
-            component={() => <TablePaginationComponent table={table} />}
-            count={table.getFilteredRowModel().rows.length}
-            rowsPerPage={table.getState().pagination.pageSize}
-            page={table.getState().pagination.pageIndex}
-            onPageChange={(_, page) => {
-              table.setPageIndex(page);
-            }}
-          />
-        </CardContent>
+        </div>
+        <div className="overflow-x-auto">
+          <table className={tableStyles.table}>
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th key={header.id}>
+                      {header.isPlaceholder ? null : (
+                        <div
+                          className={classnames({
+                            "flex items-center": header.column.getIsSorted(),
+                            "cursor-pointer select-none":
+                              header.column.getCanSort(),
+                          })}
+                          onClick={header.column.getToggleSortingHandler()}
+                        >
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                          {{
+                            asc: <i className="tabler-chevron-up text-xl" />,
+                            desc: <i className="tabler-chevron-down text-xl" />,
+                          }[header.column.getIsSorted() as "asc" | "desc"] ??
+                            null}
+                        </div>
+                      )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </thead>
+            <tbody>
+              {table.getRowModel().rows.map((row) => (
+                <tr key={row.id}>
+                  {row.getVisibleCells().map((cell) => (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <TablePagination
+          component={() => <TablePaginationComponent table={table} />}
+          count={table.getFilteredRowModel().rows.length}
+          rowsPerPage={table.getState().pagination.pageSize}
+          page={table.getState().pagination.pageIndex}
+          onPageChange={(_, page) => {
+            table.setPageIndex(page);
+          }}
+        />
       </Card>
 
       {/* Add Role Drawer */}
